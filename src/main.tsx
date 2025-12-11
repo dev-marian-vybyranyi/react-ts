45035a13199d958a0002a2c0138918d9d2c8ef9e
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-11 14:08:01 +0200

    Add theme configuration and integrate Chakra UI system; update provider component

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
+    <Provider defaultTheme="light">
       <App />
     </Provider>
   </StrictMode>
