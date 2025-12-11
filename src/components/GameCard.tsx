47e7243cca7a744b3a5bdacf2eea4322bcb774e1
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-11 16:46:00 +0200

    Add getCropedImageUrl utility for image processing and update GameCard to use it

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import type { Game } from "@/hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIvonList from "./PlatformIvonList";
 import CriticScore from "./CriditScore";
+import getCropedImageUrl from "@/services/image-url";
 
 interface GameCardProps {
   game: Game;
@@ -10,7 +11,7 @@ interface GameCardProps {
 const GameCard = (props: GameCardProps) => {
   return (
     <Card.Root borderRadius={10} overflow="hidden">
-      <Image src={props.game.background_image} />
+      <Image src={getCropedImageUrl(props.game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{props.game.name}</Heading>
         <HStack justifyContent="space-between">
