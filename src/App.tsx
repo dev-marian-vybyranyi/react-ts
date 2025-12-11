eef422608b87ea2b3ed8c16c15d557f459191303
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-11 13:42:00 +0200

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button>Click me</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" " main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <GridItem
+          area="aside"
+          bg="gold"
+          display={{ base: "none", lg: "block" }}
+        >
+          Aside
+        </GridItem>
+        <GridItem area="main" bg="dodgerblue ">
+          Main
+        </GridItem>
+      </Grid>
     </>
   );
 }
