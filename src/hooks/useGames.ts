5989cae74a4ebf582085610389e7b1b7f2bdfc80
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-15 10:04:08 +0200

    refactor: Introduce a generic `APIClient` class to centralize and simplify data fetching in hooks.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,32 +1,31 @@
 import type { GameQuery } from "@/App";
-import { type FetchResponse } from "../services/api-client";
+import APIClient, { type FetchResponse } from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "@/services/api-client";
 import type { Platform } from "./usePlatforms";
 
+const apiClient = new APIClient<Game>("/games");
+
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: { platform: Platform }[];
-    metacritic: number;
-    rating_top: number;
-};
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
+  rating_top: number;
+}
 
 const useGames = (gameQuery: GameQuery) =>
-    useQuery<FetchResponse<Game>, Error>({
-        queryKey: ["games", gameQuery],
-        queryFn: () =>
-            apiClient
-                .get<FetchResponse<Game>>("/games", {
-                    params: {
-                        genres: gameQuery.genre?.id,
-                        parent_platforms: gameQuery.platform?.id,
-                        ordering: gameQuery.sortOrder,
-                        search: gameQuery.searchText,
-                    },
-                })
-                .then(res => res.data),
-    });
+  useQuery<FetchResponse<Game>, Error>({
+    queryKey: ["games", gameQuery],
+    queryFn: () =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+        },
+      }),
+  });
 
 export default useGames;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,22 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "@/services/api-client";
-import type { FetchResponse } from "../services/api-client";
+import APIClient from "@/services/api-client";
 import genres from "@/data/genres";
 
-export interface Genre {
-    id: number;
-    name: string;
-    image_background: string;
-};
+const apiClient = new APIClient<Genre>("/genres");
 
-const useGenres = () => useQuery({
+export interface Genre {
+  id: number;
+  name: string;
+  image_background: string;
+}
+
+const useGenres = () =>
+  useQuery({
     queryKey: ["genres"],
-    queryFn: () =>
-        apiClient
-            .get<FetchResponse<Genre>>("/genres")
-            .then(res => res.data),
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24 hours
-    initialData: { count: genres.length, results: genres }
-});
+    initialData: { count: genres.length, results: genres },
+  });
 
 export default useGenres;
