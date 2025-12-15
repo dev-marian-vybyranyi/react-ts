cefa9086a4d6c67dbee05beb14ffb97069faf5bf
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-15 17:09:06 +0200

    Extract Game, Platform, and Genre interfaces into dedicated entity files and update their import paths.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,4 @@
-import type { Game } from "@/hooks/useGames";
+import type { Game } from "@/entities/Game";
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformIvonList from "./PlatformIconList";
 import CriticScore from "./CriditScore";
